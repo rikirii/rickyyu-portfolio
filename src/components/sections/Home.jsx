ad3d9247b5156feb27bb50762dce36ae9dc07e5f
Ricky <ry5665@rit.edu>
2025-05-10 00:39:22 -0400

    added some changes

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -6,11 +6,11 @@ export const Home = () => {
         <RevealOnScroll>
             <div className="text-center z-10 px-4">
                 <h1 className="text-5xl  md:text-7xl font-bold mb-6 bg-gradient-to-r from-teal-400 to-sky-500 bg-clip-text text-transparent leading-right">
-                    Hi, I'm Kiri
+                    Hi, I'm Ricky Yu
                 </h1>
 
                 <p className="text-[#EEF1E5] text-lg mb-8 max-w-lg mx-auto">
-                    I'm an undergradutae at RIT, seeking to expand my skills and experiences.
+                    a game developer undergraduate student at RIT.
                 </p>
                 <div className="flex justify-center space-x-4">
                     <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden 
@@ -18,10 +18,10 @@ export const Home = () => {
                         View Projects
                     </a>
 
-                    <a href="#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 
+                    {/* <a href="#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 
                     hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10">
                         Contact me
-                    </a>
+                    </a> */}
                 </div>
             </div>
         </RevealOnScroll>
